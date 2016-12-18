6310c9f4c52898665a57ec186366ddf3ad7f37bb
Deep Sukhwani <deepsukhwani@gmail.com>
2016-12-18 07:35:42 +0530

    Start using templates

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Rango says, Hey there Partner! <a href='/rango/about'>About Page</a>")
+    # return HttpResponse("Rango says, Hey there Partner! <a href='/rango/about'>About Page</a>")
+    context_dict = {'boldmessage': 'Some bold message on About page'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
