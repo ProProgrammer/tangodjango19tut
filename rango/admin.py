ba5741b430b1d858db25cc3e901b37713af1db73
Deep Sukhwani <deepsukhwani@gmail.com>
2016-12-24 08:47:04 +0530

    Prepopulate the slug field in rango/admin.py

--- rango/admin.py
+++ rango/admin.py
@@ -9,6 +9,22 @@ class CategoryAdmin(admin.ModelAdmin):
 
     list_display = ['name', 'page_count', 'views', 'likes']
 
+    """
+    We have a problem with slug field being user editable and not pre-populated.
+    Currently when creating a new category, say "Python User Groups" Django will not let you save it unless you fill
+    in the slug field too.
+    While we could type in python-user-groups this is error prone.
+    It would be better to have the slug automatically generated
+
+    (Another way would have been to set blank=True on slug field as:
+    slug = models.SlugField(blank=True
+    But this is not a good idea either)
+
+    Lets customize the admin interface so that it automatically pre-populates the slug field as you type in the
+    category name
+    """
+    prepopulated_fields = {'slug': ('name', )}
+
 
 class PageAdmin(admin.ModelAdmin):
     def page_url(self, obj):
