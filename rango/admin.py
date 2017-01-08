72f3add6903e0d01e3a55fc713861cbbfb37adf2
Deep Sukhwani <deepsukhwani@gmail.com>
2017-01-08 13:24:15 +0530

    Ch.9 - Add UserProfile to admin and create migration for it

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -37,3 +37,4 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
