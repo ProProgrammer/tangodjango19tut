3f046cf6226312dcdf16b6acdb7ad3d05b1f753e
Deep Sukhwani <deepsukhwani@gmail.com>
2017-01-08 16:18:54 +0530

    Ch.9 Add register url mapping to rango/urls.py

--- rango/urls.py
+++ rango/urls.py
@@ -6,8 +6,12 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^add_category/$', views.add_category, name='add_category'),
+
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
+
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
+
+    url(r'^register/$', views.register, name='register'),
 ]
 """
 We have added a rather complex entry that will invoke view.show_category() when the URL pattern r'^category/(
