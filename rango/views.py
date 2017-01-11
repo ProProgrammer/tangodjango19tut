790ac121c728823e70f92e970f65a6e0ce8323a6
Deep Sukhwani <deepsukhwani@gmail.com>
2017-01-11 18:53:36 +0530

    Ch.9 Exercises - Provide informative error messages on failed login

--- rango/views.py
+++ rango/views.py
@@ -223,12 +223,14 @@ def user_login(request):
                 login(request, user)
                 return HttpResponseRedirect(reverse('rango:index'))
             else:
+                error = "Your rango account is disabled"
                 # An inactive account was used - no logging in!
-                return HttpResponse("Your Rango account is disabled.")
+                return render(request, 'rango/login.html', {'error': error})
         else:
             # Bad login details were provided. So we can't log the user in.
-            print "Invalid login details: {0}, {1}".format(username, password)
-            return HttpResponse("Invalid login details supplied.")
+            error = "Invalid Login details"
+            return render(request, 'rango/login.html', {'error': error})
+            # return HttpResponse("Invalid login details supplied.")
 
     # This request is not a HTTP POST, so display the login form.
     # This scenario most likely be a HTTP GET.
